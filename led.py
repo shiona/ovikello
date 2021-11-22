810184106253b6280a5531a4ec14b7549fab81bb
shiona <->
2021-11-22 13:59:17 +0000

    Refactor led code, make sure it starts black

--- led.py
+++ led.py
@@ -6,6 +6,10 @@ from rpi_ws281x import Adafruit_NeoPixel, Color
 import logging
 
 class Led():
+
+	BLACK = Color(0, 0, 0)
+	RED   = Color(255, 0, 0)
+
 	def __init__(self, led_pin, led_count):
 		LED_FREQ_HZ = 800000
 		LED_DMA = 10
@@ -21,17 +25,20 @@ class Led():
 	                                       LED_DMA, LED_INVERT, LED_BRIGHTNESS)
 		self.strip.begin()
 
+		self.set_color(self.BLACK)
+
+	def set_color(self, color):
+		for i in range(self.led_count):
+			self.strip.setPixelColor(i, color)
+		self.strip.show()
+
+
 	def alert(self):
 		blink_sleep_time = 1/(2*self.BLINK_FREQ)
 		logging.info("led alert!")
 		for c in range(math.ceil(self.BLINK_TIME/self.BLINK_FREQ)):
-
-			for i in range(self.led_count):
-				self.strip.setPixelColor(i, Color(255, 0, 0))
-			self.strip.show()
+			self.set_color(self.RED)
 			time.sleep(blink_sleep_time)
 
-			for i in range(self.led_count):
-				self.strip.setPixelColor(i, Color(0, 0, 0))
-			self.strip.show()
+			self.set_color(self.BLACK)
 			time.sleep(blink_sleep_time)
