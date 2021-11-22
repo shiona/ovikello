f183aa32ddb0ba8666786b2fc2a4563bef798fdc
shiona <->
2021-11-22 13:37:11 +0000

    use command instead of inlike keyboard

--- bot.py
+++ bot.py
@@ -1,34 +1,20 @@
 from telegram.ext import Updater, CallbackQueryHandler, CommandHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 
+CHECKMARK = '\u2713'
+
 class DoorbellBot():
 
 	def __init__(self, token, led):
-		led = led
-		EMOJI_DOOR = "\uD83D\uDEAA"
-		DOOR_CALLBACK_DATA = "sesam"
-
 		self.updater = Updater(token=token, use_context=True)
 		dispatcher = self.updater.dispatcher
 
-		keyboard = [[ InlineKeyboardButton(EMOJI_DOOR, callback_data=DOOR_CALLBACK_DATA) ]]
-		keyboard_markup = InlineKeyboardMarkup(keyboard)
+		def ovikello(update, context):
+			update.message.reply_text(CHECKMARK)
+			led.alert()
 
-		def start(update, context):
-			update.message.reply_text("Mo", reply_markup=keyboard_markup)
-	
-		def button(update, context):
-			query = update.callback_query
-			query.answer()
-
-			data = query.data
-
-			if data == DOOR_CALLBACK_DATA:
-				led.alert()
-
-		start_handler = CommandHandler("start", start)
-		dispatcher.add_handler(start_handler)
-		dispatcher.add_handler(CallbackQueryHandler(button))
+		ovikello_handler = CommandHandler("ovikello", ovikello)
+		dispatcher.add_handler(ovikello_handler)
 
 	def start(self):
 		self.updater.start_polling()
